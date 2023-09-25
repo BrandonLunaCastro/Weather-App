d67c97f582168588ba0785a8dbcbd69e2a41d39b
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-09-25 19:59:12 -0300

    fixed: add basic styles and insert content from api on the html

--- src/modules/content.js
+++ src/modules/content.js
@@ -1,6 +1,35 @@
 import { getDailyForecast, getWeeklyForecast } from "./getData.js"
 
-export default async function loadContent () {
-    console.log(await getDailyForecast())
-    console.log(await getWeeklyForecast())
+const showContent = async (daily, weekly) => {
+    const main = document.querySelector("main")
+    const locationTitle = document.querySelector(".location")
+    const country = document.querySelector(".country")
+    const tempCelcius = document.querySelector(".temp")
+    const icon = document.getElementById("icon")
+    const imageZone = document.getElementById("image__zone")
+    const zone = document.querySelector(".zone")
+    if (!daily && !weekly) {
+        daily = await getDailyForecast()
+        weekly = await getWeeklyForecast()
+    }
+    zone.innerText = `${daily.location.name}, ${daily.location.region} ${daily.currentWeather.temp_c}°c`
+    imageZone.src = daily.currentWeather.condition.icon
+    locationTitle.innerText = daily.location.name
+    country.innerText = daily.location.country
+    tempCelcius.innerText = `${daily.currentWeather.temp_c}°C`
+    icon.src = daily.currentWeather.condition.icon
+    console.log(daily)
+    console.log(weekly)
+}
+
+const searchLocation = async (e) => {
+    const location = e.target.value
+    const dailyForecast = await getDailyForecast(location)
+    const weeklyForecast = await getWeeklyForecast(location)
+    showContent(dailyForecast, weeklyForecast)
+}
+export default function loadContent () {
+    const search = document.getElementById("search")
+    showContent()
+    search.addEventListener("change", searchLocation)
 }
--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,18 +1,17 @@
-const getWeeklyForecast = async () => {
+const getWeeklyForecast = async (location = "mendoza") => {
     const res = await fetch(
-        "http://api.weatherapi.com/v1/forecast.json?key=0804b0f642fd40ccb16202934232209&days=7&q=mendoza"
+        `http://api.weatherapi.com/v1/forecast.json?key=0804b0f642fd40ccb16202934232209&days=7&q=${location}`
     )
     const json = await res.json()
     return {
         weekly: json.forecast.forecastday
     }
 }
-const getDailyForecast = async () => {
-    const response = await fetch(
-        "http://api.weatherapi.com/v1/forecast.json?key=0804b0f642fd40ccb16202934232209&q=mendoza"
-    )
+const getDailyForecast = async (location = "mendoza") => {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=0804b0f642fd40ccb16202934232209&q=${location}`)
     const json = await response.json()
     return {
+        location: json.location,
         hour: json.forecast.forecastday[0].hour,
         day: json.forecast.forecastday[0].day,
         currentWeather: json.current
