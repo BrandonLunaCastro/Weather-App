498fc4f479cb5e076bb879251bd4746d64666766
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-09-28 11:25:04 -0300

    BREAKING CHANGE: refactor function background
    fixed: add fetch to call for images  of the  pexels API

--- src/modules/background.js
+++ src/modules/background.js
@@ -1,8 +1,21 @@
+function randomNumber() {
+    return Math.round(Math.random() * (15 - 0) + 0);
+}
+
 export default function changeBackground(data) {
-    const request = fetch(`https://api.giphy.com/v1/gifs/translate?api_key=KTFltm3ISnIkpTY4ANfoLWU31YCWkAeN&s=${data}`, { mode: "cors" });
+    const imageNum = randomNumber();
+    const background = document.querySelector("html");
+    const request = fetch(`https://api.pexels.com/v1/search?query=${data}`, {
+        headers: { Authorization: "8rdzCB2djNs8QYBtzTTEhhf2WtfnxhpF37ND4S6TS0hgJSHR7T3aszYs" },
+        mode: "cors",
+    });
     request.then((res) => res.json())
         .then((json) => {
-            const background = document.querySelector("html");
-            background.style.backgroundImage = `url(${json.data.images.original.url})`;
+            background.style.backgroundImage = `url(${json.photos[imageNum].src.original})`;
+            document.querySelector(".photographer").innerText = `Photo by ${json.photos[imageNum].photographer}`;
+        })
+        .catch(() => {
+            background.style.backgroundImage = "url(https://images.pexels.com/photos/531756/pexels-photo-531756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+            document.querySelector(".photographer").innerText = "Photo by Pixabay";
         });
 }
